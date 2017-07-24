459e7448107066bf51589ef523ed67231cad921b
Kerbores <Kerbores@gmail.com>
2017-07-24 10:45:18 +0800

    init

---
+++ axe/axe-sigar/src/main/java/club/zhcs/sigar/SigarFactory.java
@@ -0,0 +1,37 @@
+package club.zhcs.sigar;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.hyperic.sigar.Sigar;
+import org.nutz.lang.Files;
+import org.nutz.resource.NutResource;
+import org.nutz.resource.Scans;
+
+/**
+ * @author kerbores
+ *
+ */
+public class SigarFactory {
+
+	final static String LIB_PATH = String.format("%s%s%s", System.getProperty("user.home"), File.separator, "sigar_lib");
+
+	static {
+		List<NutResource> resources = Scans.me().scan("sigar", ".(so|sl|dylib|dll|lib)$");
+		for (NutResource resource : resources) {
+			try {
+				Files.write(String.format("%s/%s", LIB_PATH, resource.getName()), resource.getInputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		String path = System.getProperty("java.library.path");
+		path += File.pathSeparator + LIB_PATH;
+		System.setProperty("java.library.path", path);
+	}
+
+	public static Sigar load() {
+		return new Sigar();
+	}
+}
