161354d5b1f43823eeb151983a25b4c99332b7ae
王贵源 <wangguiyuan@sinosoft.com.cn>
2018-06-02 22:11:27 +0800

    add:新版本,大调整

---
+++ axe/axe-swagger/src/main/java/club/zhcs/swagger/Swagger2Configuration.java
@@ -0,0 +1,21 @@
+package club.zhcs.swagger;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.swagger2.configuration.Swagger2DocumentationConfiguration;
+
+/**
+ * @author 王贵源<wangguiyuan@sinosoft.com.cn>
+ *
+ * @date 2018-06-02 21:46:20
+ */
+@Configuration
+@ConditionalOnProperty(name = "swagger.enabled", matchIfMissing = true)
+@Import({
+         Swagger2DocumentationConfiguration.class,
+         BeanValidatorPluginsConfiguration.class
+})
+public class Swagger2Configuration {}
