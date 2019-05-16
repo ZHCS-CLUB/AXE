2be6e6e113296fd166f8c26ef0fa259e119db424
王贵源 <kerbores@gmail.com>
2019-05-16 13:54:05 +0800

    upgrade: 升级版本

--- axe/axe-chinare-crm/src/main/java/com/chinare/axe/apm/APMAppender.java
+++ axe/axe-chinare-crm/src/main/java/com/chinare/axe/apm/APMAppender.java
@@ -1,7 +1,8 @@
 package com.chinare.axe.apm;
 
-import java.util.Arrays;
 import java.util.Date;
+
+import org.nutz.json.Json;
 
 /**
  * @author kerbores
@@ -9,170 +10,148 @@ import java.util.Date;
  */
 public interface APMAppender {
 
-    public static class APMLog {
+	public static class APMLog {
 
-        String url;
+		String url;
 
-        String tag;
+		String tag;
 
-        String user;
+		String user;
 
-        Date actionTime;
+		Date actionTime;
 
-        long actionDuration;
+		long actionDuration;
 
-        Object[] args;
+		Object[] args;
 
-        Object retuenObj;
+		Object retuenObj;
 
-        boolean exception;
+		boolean exception;
 
-        /**
-         * @return the url
-         */
-        public String getUrl() {
-            return url;
-        }
+		/**
+		 * @return the url
+		 */
+		public String getUrl() {
+			return url;
+		}
 
-        /**
-         * @param url
-         *            the url to set
-         */
-        public void setUrl(String url) {
-            this.url = url;
-        }
+		/**
+		 * @param url the url to set
+		 */
+		public void setUrl(String url) {
+			this.url = url;
+		}
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see java.lang.Object#toString()
-         */
-        @Override
-        public String toString() {
-            return "APMLog [tag="
-                   + tag
-                   + ", user="
-                   + user
-                   + ", actionTime="
-                   + actionTime
-                   + ", actionDuration="
-                   + actionDuration
-                   + ", args="
-                   + Arrays.toString(args)
-                   + ", retuenObj="
-                   + retuenObj
-                   + ", exception="
-                   + exception
-                   + "]";
-        }
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Object#toString()
+		 */
+		@Override
+		public String toString() {
+			return Json.toJson(this);
+		}
 
-        /**
-         * @return the exception
-         */
-        public boolean isException() {
-            return exception;
-        }
+		/**
+		 * @return the exception
+		 */
+		public boolean isException() {
+			return exception;
+		}
 
-        /**
-         * @param exception
-         *            the exception to set
-         */
-        public void setException(boolean exception) {
-            this.exception = exception;
-        }
+		/**
+		 * @param exception the exception to set
+		 */
+		public void setException(boolean exception) {
+			this.exception = exception;
+		}
 
-        /**
-         * @return the tag
-         */
-        public String getTag() {
-            return tag;
-        }
+		/**
+		 * @return the tag
+		 */
+		public String getTag() {
+			return tag;
+		}
 
-        /**
-         * @param tag
-         *            the tag to set
-         */
-        public void setTag(String tag) {
-            this.tag = tag;
-        }
+		/**
+		 * @param tag the tag to set
+		 */
+		public void setTag(String tag) {
+			this.tag = tag;
+		}
 
-        /**
-         * @return the user
-         */
-        public String getUser() {
-            return user;
-        }
+		/**
+		 * @return the user
+		 */
+		public String getUser() {
+			return user;
+		}
 
-        /**
-         * @param user
-         *            the user to set
-         */
-        public void setUser(String user) {
-            this.user = user;
-        }
+		/**
+		 * @param user the user to set
+		 */
+		public void setUser(String user) {
+			this.user = user;
+		}
 
-        /**
-         * @return the actionTime
-         */
-        public Date getActionTime() {
-            return actionTime;
-        }
+		/**
+		 * @return the actionTime
+		 */
+		public Date getActionTime() {
+			return actionTime;
+		}
 
-        /**
-         * @param actionTime
-         *            the actionTime to set
-         */
-        public void setActionTime(Date actionTime) {
-            this.actionTime = actionTime;
-        }
+		/**
+		 * @param actionTime the actionTime to set
+		 */
+		public void setActionTime(Date actionTime) {
+			this.actionTime = actionTime;
+		}
 
-        /**
-         * @return the actionDuration
-         */
-        public long getActionDuration() {
-            return actionDuration;
-        }
+		/**
+		 * @return the actionDuration
+		 */
+		public long getActionDuration() {
+			return actionDuration;
+		}
 
-        /**
-         * @param actionDuration
-         *            the actionDuration to set
-         */
-        public void setActionDuration(long actionDuration) {
-            this.actionDuration = actionDuration;
-        }
+		/**
+		 * @param actionDuration the actionDuration to set
+		 */
+		public void setActionDuration(long actionDuration) {
+			this.actionDuration = actionDuration;
+		}
 
-        /**
-         * @return the args
-         */
-        public Object[] getArgs() {
-            return args;
-        }
+		/**
+		 * @return the args
+		 */
+		public Object[] getArgs() {
+			return args;
+		}
 
-        /**
-         * @param args
-         *            the args to set
-         */
-        public void setArgs(Object[] args) {
-            this.args = args;
-        }
+		/**
+		 * @param args the args to set
+		 */
+		public void setArgs(Object[] args) {
+			this.args = args;
+		}
 
-        /**
-         * @return the retuenObj
-         */
-        public Object getRetuenObj() {
-            return retuenObj;
-        }
+		/**
+		 * @return the retuenObj
+		 */
+		public Object getRetuenObj() {
+			return retuenObj;
+		}
 
-        /**
-         * @param retuenObj
-         *            the retuenObj to set
-         */
-        public void setRetuenObj(Object retuenObj) {
-            this.retuenObj = retuenObj;
-        }
+		/**
+		 * @param retuenObj the retuenObj to set
+		 */
+		public void setRetuenObj(Object retuenObj) {
+			this.retuenObj = retuenObj;
+		}
 
-    }
+	}
 
-    void append(APMLog log);
+	void append(APMLog log);
 
 }
