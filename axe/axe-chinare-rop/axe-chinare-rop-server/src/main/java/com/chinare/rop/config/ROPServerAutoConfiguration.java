6b77291bf9e73334ed82b2000b5597131fe5cfd7
王贵源 <kerbores@gmail.com>
2019-09-27 10:29:00 +0800

    fix: servlet版本兼容

--- axe/axe-chinare-rop/axe-chinare-rop-server/src/main/java/com/chinare/rop/config/ROPServerAutoConfiguration.java
+++ axe/axe-chinare-rop/axe-chinare-rop-server/src/main/java/com/chinare/rop/config/ROPServerAutoConfiguration.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -47,6 +48,9 @@ public class ROPServerAutoConfiguration {
         registration.setFilter(new Filter() {
 
             @Override
+            public void destroy() {}
+
+            @Override
             public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
                 ServletRequest requestWrapper = null;
                 if (request instanceof HttpServletRequest) {
@@ -58,6 +62,9 @@ public class ROPServerAutoConfiguration {
                     chain.doFilter(requestWrapper, response);
                 }
             }
+
+            @Override
+            public void init(FilterConfig filterConfig) throws ServletException {}
         });
         registration.addUrlPatterns(properties.getRopPath());
         registration.setOrder(1);
