8953f948135bbc90e43a805e7becce62d7edde33
王贵源 <wangguiyuan@chinarecrm.com.cn>
2019-10-16 15:36:32 +0800

    add: 增加用户自主跳过捡取的逻辑判断扩展点

--- axe/axe-chinare-crm/src/main/java/com/chinare/axe/auth/AbstractAuthService.java
+++ axe/axe-chinare-crm/src/main/java/com/chinare/axe/auth/AbstractAuthService.java
@@ -14,74 +14,83 @@ import org.nutz.lang.Strings;
  */
 public abstract class AbstractAuthService implements AuthService {
 
-    static final String AUTHORIZATION_KEY = "Authorization";
+	static final String AUTHORIZATION_KEY = "Authorization";
 
-    HttpServletRequest request;
+	protected HttpServletRequest request;
 
-    /**
-     * 
-     * @param request
-     *            http 请求
-     */
-    public AbstractAuthService(HttpServletRequest request) {
-        this.request = request;
-    }
+	/**
+	 * 
+	 * @param request http 请求
+	 */
+	public AbstractAuthService(HttpServletRequest request) {
+		this.request = request;
+	}
 
-    @Override
-    public boolean authentication(List<String> withoutAuthenticationUrlRegulars) {
-        if (pass(request.getRequestURI(), withoutAuthenticationUrlRegulars)) {
-            return true;
-        }
-        return user() != null;
-    }
+	public HttpServletRequest getRequest() {
+		return request;
+	}
 
-    /**
-     * @param uri
-     *            请求地址
-     * @param withoutAuthenticationUrlRegulars
-     *            放行url正则
-     * @return 是否放行
-     */
-    protected boolean pass(String uri, List<String> withoutAuthenticationUrlRegulars) {
-        for (String regular : withoutAuthenticationUrlRegulars) {
-            if (Pattern.matches(regular, uri)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	@Override
+	public boolean authentication(List<String> withoutAuthenticationUrlRegulars) {
+		if (skip()) {
+			return true;
+		}
+		if (pass(request.getRequestURI(), withoutAuthenticationUrlRegulars)) {
+			return true;
+		}
+		return user() != null;
+	}
 
-    @Override
-    public String token() {
-        String token = request.getHeader(AUTHORIZATION_KEY);
-        if (token == null) {
-            Cookie[] cookies = request.getCookies();
-            if (cookies != null) {
-                for (Cookie ele : cookies) {
-                    if (Strings.equalsIgnoreCase(ele.getName(), AUTHORIZATION_KEY)) {
-                        token = ele.getValue();
-                    }
-                }
-            }
+	/**
+	 * @param uri                              请求地址
+	 * @param withoutAuthenticationUrlRegulars 放行url正则
+	 * @return 是否放行
+	 */
+	protected boolean pass(String uri, List<String> withoutAuthenticationUrlRegulars) {
+		for (String regular : withoutAuthenticationUrlRegulars) {
+			if (Pattern.matches(regular, uri)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-        }
-        if (token == null) {
-            token = request.getParameter(AUTHORIZATION_KEY);
-        }
-        return token;
-    }
+	@Override
+	public String token() {
+		String token = request.getHeader(AUTHORIZATION_KEY);
+		if (token == null) {
+			Cookie[] cookies = request.getCookies();
+			if (cookies != null) {
+				for (Cookie ele : cookies) {
+					if (Strings.equalsIgnoreCase(ele.getName(), AUTHORIZATION_KEY)) {
+						token = ele.getValue();
+					}
+				}
+			}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.chinare.axe.auth.AuthService#userName()
-     */
-    @Override
-    public String userName() {
-        if (token() == null) {
-            return null;
-        }
-        return JwtUtil.getUsername(token());
-    }
+		}
+		if (token == null) {
+			token = request.getParameter(AUTHORIZATION_KEY);
+		}
+		return token;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.chinare.axe.auth.AuthService#userName()
+	 */
+	@Override
+	public String userName() {
+		if (token() == null) {
+			return null;
+		}
+		return JwtUtil.getUsername(token());
+	}
+
+	@Override
+	public boolean skip() {
+		return false;
+	}
 
 }
